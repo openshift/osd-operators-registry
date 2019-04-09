a54dbb903dea2c63092896be67ab174b91ea0df1
Lisa Seelye <lseelye@redhat.com>
2019-04-09 16:19:48 -0400

    Address Naveen's feedback in checkout-operator.mk
    Allow `SKIP_GITREFRESH` to be used to skip doing `git reset` and `git
    pull` on the checked out copies of the source code.
    
    Signed-off-by: Lisa Seelye <lseelye@redhat.com>

--- checkout-operator.mk
+++ checkout-operator.mk
@@ -6,6 +6,10 @@ define checkout_operator
 	if [[ ! -d $(SOURCE_DIR)/$(2) ]]; then \
 		git clone -b master https://github.com/$(1)/$(2).git $(SOURCE_DIR)/$(2) ;\
 	else \
-		cd $(SOURCE_DIR)/$(2) git reset --hard && git pull --force && cd ../.. ;\
+		if [[  -z "$${SKIP_GITREFRESH}" ]]; then \
+			cd $(SOURCE_DIR)/$(2) git reset --hard && git pull --force && cd ../.. ;\
+		else \
+			echo "SKIP_GITREFRESH set, skipping git refresh for $(1)$(2)" ;\
+		fi ;\
 	fi
 endef
